b459b1c119ffc47f5dcf5544c4a6a342d6102acc
professorDeveloper <azamovhud007@outlook.com>
2024-03-17 00:16:49 +0500

    Updated Profile Screen

--- lib/screens/order/orders_screen.dart
+++ lib/screens/order/orders_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:medhome/screens/profile/profile_screen.dart';
 
@@ -19,7 +20,6 @@ class _OrdersScreenState extends State<OrdersScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.gray1,
-
       appBar: _appBar(contxt: context),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
@@ -27,23 +27,25 @@ class _OrdersScreenState extends State<OrdersScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(left: 25,top: 15),
-              child: Text("Buyurtmalar",style: GoogleFonts.rubik(fontWeight:FontWeight.w500,fontSize: 20,color: AppColor.textColor)),
-
+              padding: const EdgeInsets.only(left: 20, top: 15),
+              child: Text("Buyurtmalar",
+                  style: GoogleFonts.rubik(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: AppColor.textColor)),
             ),
-            SizedBox(height: 12,),
+            SizedBox(
+              height: 10,
+            ),
             customOrderItem(),
-
           ],
         ),
       ),
     );
   }
-  AppBar _appBar(
-      {required BuildContext contxt}) =>
-      AppBar(
-        elevation:0,
 
+  AppBar _appBar({required BuildContext contxt}) => AppBar(
+        elevation: 0,
         scrolledUnderElevation: 1,
         toolbarHeight: 60,
         backgroundColor: Colors.grey.shade200,
@@ -54,9 +56,11 @@ class _OrdersScreenState extends State<OrdersScreen> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(width: 5,),
+                SizedBox(
+                  width: 5,
+                ),
                 IconButton(
-                    onPressed: (){
+                    onPressed: () {
                       Scaffold.of(contxt).openDrawer();
                       Scaffold.of(context).openDrawer();
                       print('"asdasdasdasdasdasd');
@@ -100,49 +104,167 @@ class _OrdersScreenState extends State<OrdersScreen> {
                       )),
                 ),
                 IconButton(
-                    onPressed: (() => {
-                      openScreen(context, MyProfile())
-                    }),
+                    onPressed: (() => {openScreen(context, MyProfile())}),
                     icon: Icon(
                       CupertinoIcons.person_alt_circle,
                       size: 27,
                       color: AppColor.red4,
                     )),
-                SizedBox(width: 5,),
-
+                SizedBox(
+                  width: 5,
+                ),
               ]),
         ),
-
         automaticallyImplyLeading: false,
       );
 
-  Card customOrderItem(){
+  Card customOrderItem() {
     return Card(
-      shape:RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(11))
-      ) ,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(11))),
       borderOnForeground: true,
-      elevation: 1,
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+      shadowColor: AppColor.gray2,
+      elevation: 1.5,
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
       color: Colors.white,
-      child: InkWell(
-        onTap: (){
-          print('asdasd');
-        },
-        borderRadius: BorderRadius.all(Radius.circular(11)),
-        child: Container(
+      child: Container(
           width: double.infinity,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
+                padding: const EdgeInsets.only(left: 10, top: 8),
+                child: Text(
+                  "Bemor",
+                  style: GoogleFonts.rubik(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      color: AppColor.textColor),
+                ),
+              ),
+              Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("Bemor",style: GoogleFonts.rubik(fontSize: 15,fontWeight: FontWeight.w500,color: AppColor.textColor),),
-              )
+                child: Container(
+                  height: 70,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: AppColor.gray2),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 8,
+                      ),
+                      SvgPicture.asset(
+                        AppImages.icContact,
+                        height: 34,
+                        width: 32,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Ism Familiya Otchestvo",
+                        style: GoogleFonts.rubik(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: AppColor.black),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColor.gray2),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                      columnText("Hizmat Turi", "Kardiolog"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      columnText("Manzil", "Toshkent shahar Chilonzor tumani"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      columnText("Narx", "200 000 so’m"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      columnText("Shifokor", "Ism Familiya Otchestvo"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10, right: 10),
+                child: Container(
+                  height: 60,
+                  width: double.infinity,
+                  child: MaterialButton(
+                    elevation: 0,
+                    highlightElevation: 0,
+                    focusElevation: 0,
+                    onPressed: () async {},
+                    color: AppColor.red1,
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: const Text("Bekor Qilish"),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
             ],
-          )
-        ),
-      ),
+          )),
     );
+  }
 
+  Widget columnText(String topText, String mainText) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(topText,
+              style: GoogleFonts.rubik(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xff5B5B5B))),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(mainText,
+              style: GoogleFonts.rubik(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: AppColor.black)),
+        ),
+      ],
+    );
   }
 }
